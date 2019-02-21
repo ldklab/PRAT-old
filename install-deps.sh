ad7d35f0394ddb22f81d1412bbebce6295dbadde
Ryan Williams <williams.ry@husky.neu.edu>
2019-02-21 15:21:56 -0500

    evolving dep install script

---
+++ install-deps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Get dependencies for angr.
+apt-get install python3-dev libffi-dev build-essential virtualenvwrapper
+
+#mkvirtualenv --python=$(which python3) angr && pip install angr
