5f84358aae96a254fd42241ec4765e3e44331f9d
Ryan Williams <williams.ry@husky.neu.edu>
2019-04-05 13:54:01 -0400

    Switched to tokenized case

--- word2vec/scripts/create-rfc-vector.sh
+++ word2vec/scripts/create-rfc-vector.sh
@@ -5,8 +5,8 @@ BIN_DIR=../bin
 SRC_DIR=../src
 
 # Make this a cmd line arg later.
-TEXT_DATA=$DATA_DIR/merged-rfc-1000
-VECTOR_DATA=$DATA_DIR/merged-rfc-1000-skipgram-vector.bin
+TEXT_DATA=$DATA_DIR/merged-rfc-2000-tokenized
+VECTOR_DATA=$DATA_DIR/merged-rfc-2000-cbow-tokenized-vector.bin
 
 if [ ! -e $VECTOR_DATA ]; then
   if [ ! -e $TEXT_DATA ]; then
@@ -17,5 +17,5 @@ if [ ! -e $VECTOR_DATA ]; then
   echo -----------------------------------------------------------------------------------------------------
   echo -- Training vectors...
   # Default size is 100. 200 caused RAM issues on laptop.
-  time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
+  time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
 fi
