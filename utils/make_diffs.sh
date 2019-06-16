4045d239a116ebbfb71d5445fcfa776013f99b60
Ryan Williams <ryan.rpwilliams@gmail.com>
2019-06-16 16:12:14 -0400

    Automating process

--- utils/make_diffs.sh
+++ utils/make_diffs.sh
@@ -2,4 +2,20 @@
 
 # Script to run gcov over C files and diff them
 # with and without feature enabled.
-# TODO
+
+makeCovFiles() {
+	echo "Building project with llvm-gcov..."
+	make
+	echo "Generating gcov files..."
+	./mosquitto
+	gcov *.c
+	make clean
+}
+
+if [[ $PWD =~ "src" ]]
+then
+	makeCovFiles
+else
+	echo "Can't run in this directory. Exiting"
+	exit 1
+fi
