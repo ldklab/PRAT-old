66d17d4dae7a7047a281b06a0792518db207ed62
Ryan Williams <ryan.rpwilliams@gmail.com>
2019-07-02 19:49:04 -0400

    Will add more support later

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# First, check distro running on
+# for applicability of setup.
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  OS=$NAME
+  VER=$VERSION_ID
+  echo "OS: $OS"
+  echo "VER: $VER"
+else
+  echo "TEST"
+fi
+
 if [ "$EUID" -ne 0 ]; then
 	echo "Script needs to be run as root."
 	exit 1
