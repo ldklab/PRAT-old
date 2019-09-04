a1b687184444149c62d2e45204bc6952aa5639ff
Ryan Williams <ryan.rpwilliams@gmail.com>
2019-09-04 09:25:46 -0400

    Updated dependency installer

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -o errexit -o pipefail -o noclobber -o nounset
+trap "trap - SIGTERM" SIGINT SIGTERM EXIT
+
+if [ "$EUID" -ne 0 ]; then
+        echo "Script needs to be run as root."
+        exit 1
+fi
+
+# First, check which distro we're on || package manager installed.
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  OS=$NAME
+  VER=$VERSION_ID
+  echo "OS: $OS"
+  echo "VER: $VER"
+fi
+
+if [ $OS == "Ubuntu" ]; then
+        # Install all the dependencies required for PRAT.
+        apt-get install -y libssl-dev libwebsockets-dev uuid-dev libsystemd-dev maven
+        apt-get install -y libcunit1 libcunit1-doc libcunit1-dev gnutls-dev libwrap0 libwrap0-dev
+	
+	# Install lcov.
+	(git clone https://github.com/linux-test-project/lcov.git ; cd lcov; make install)
+
+	# Install CoreNLP.
+	#(git clone https://github.com/stanfordnlp/CoreNLP.git; cd CoreNLP; mvn package)
+
+	# Get dependencies for angr.
+	apt-get install -y python3-dev python3-pip libffi-dev build-essential virtualenvwrapper python3-tk
+	pip3 install -U -q h5py pyyaml tensorflow nltk spacy pandas
+	pip3 install -U networkx matplotlib gensim annoy seaborn sklearn
+	python -m spacy download en_core_web_sm
+	# pip install prodigy.wh1
+
+	#mkvirtualenv --python=$(which python3) angr && pip install angr
+	rm -rf lcov
+
+elif [ $OS == "Arch Linux" ]; then
+        # Use pacman.
+        # Check actual package names; these aren't set correctly.
+        pacman -S libssl-dev libwebsockets-dev uuid-dev
+else
+        echo "Unsupported distribution. Exiting."
+        exit 1
+fi
+
