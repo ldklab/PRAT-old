23682c2b440cc65f9ea968eb5af3e2e3f6782904
Ryan Williams <ryanwilliams@Ryans-MacBook-Air.local>
2019-11-17 12:56:49 -0500

    Error handling

--- utils/extract_features.pl
+++ utils/extract_features.pl
@@ -137,7 +137,7 @@ sub remove_feature {
 	print "Attempting to run [$sed_cmd]\n";
 
 	system($sed_cmd) == 0
-		or warn "Could not launch [$sed_cmd]: $! / $?\n"; # Change back to die later.
+		or die "Could not launch [$sed_cmd]: $! / $?\n"; # Change back to die later.
 
 	return;
 }
