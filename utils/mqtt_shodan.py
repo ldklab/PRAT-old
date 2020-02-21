2c9e101bdbd9bb09995ddbc08d6c49a417c0f39b
Ryan Williams <williams.ry@husky.neu.edu>
2020-02-21 16:34:21 -0500

    Handle SIGINT

--- utils/mqtt_shodan.py
+++ utils/mqtt_shodan.py
@@ -14,23 +14,24 @@ def on_message(client, userdata, msg):
 	print("[+] Topic: {} - Message: {}".format(msg.topic, msg.payload))
 
 if __name__ == "__main__":
+	try:
+		my_parser = argparse.ArgumentParser(description="Specify host IP to connect to")
 
-	my_parser = argparse.ArgumentParser(description="Specify host IP to connect to")
+		my_parser.add_argument('Host',
+								metavar='host',
+								type=str,
+								help='The host to connect to')
 
-	my_parser.add_argument('Host',
-							metavar='host',
-							type=str,
-							help='The host to connect to')
+		args = my_parser.parse_args()
 
-	args = my_parser.parse_args()
+		host = args.Host
 
-	host = args.Host
+		client = mqtt.Client(client_id= "MqttClient")
+		client.on_connect = on_connect
+		client.on_message = on_message
 
-	client = mqtt.Client(client_id= "MqttClient")
-	client.on_connect = on_connect
-	client.on_message = on_message
-
-	client.connect(host, 1883, 60)
-	client.loop_forever()
-
-	#sys.exit(0)
+		client.connect(host, 1883, 60)
+		client.loop_forever()
+	except KeyboardInterrupt:
+		print("\n[-] Exiting.")
+		sys.exit()
