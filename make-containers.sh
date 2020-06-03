a2e185d87890d5bed2b249ec0401c57643dd5d31
Ryan Williams <ryan.rpwilliams@gmail.com>
2020-06-03 12:52:47 -0400

    Updated demo driver/dockerfiles

--- make-containers.sh
+++ make-containers.sh
@@ -10,4 +10,4 @@ docker build -t prat:demo2 -f docker/demo2/Dockerfile .
 
 # Container for demo-3.
 echo "[+] Building container for demo-3..."
-docker build -t prat:demo1 -f docker/demo1/Dockerfile .
+docker build -t prat:demo3 -f docker/demo3/Dockerfile .
