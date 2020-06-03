dd70582f00cc1ea48c6740a9171ba132af5b5907
Ryan Williams <ryan.rpwilliams@gmail.com>
2020-06-03 13:06:01 -0400

    Made demo pause until key read

--- make-containers.sh
+++ make-containers.sh
@@ -8,6 +8,10 @@ docker build -t prat:demo1 -f docker/demo1/Dockerfile .
 echo "[+] Building container for demo-2..."
 docker build -t prat:demo2 -f docker/demo2/Dockerfile .
 
+# Container for demo-2-1.
+echo "[+] Building container for demo-2-1..."
+docker build -t prat:demo2-1 -f docker/demo2-1/Dockerfile .
+
 # Container for demo-3.
 echo "[+] Building container for demo-3..."
 docker build -t prat:demo3 -f docker/demo3/Dockerfile .
