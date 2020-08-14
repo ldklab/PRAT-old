985025cd218c107c879377a0d5cdd317acc913b8
Ryan Williams <ryan.rpwilliams@gmail.com>
2020-08-14 16:58:27 -0400

    Started scaffolding for Python port

--- src/PRAT.py
+++ src/PRAT.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 
-# TODO: add all the functions that 
-# `make_diffs.sh` currently has.
+import argparse
+import sys
+
+def makeMosquitto(path, feature):
+    print("[-] TODO: makeMosquitto.")
+
+def makeFFmpeg(path, feature):
+    print("[-] TODO: makeFFmpeg.")
+
+def makeDDS(path, feature):
+    print("[-] TODO: makeDDS.")
+
+def makeCM(path, feature):
+    print("[-] TODO: makeCM.")
 
 if __name__ == '__main__':
-    print("[-] TODO.")
+    # Setup the command line args for different projects.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("project", help="Directory to project to target")
+    parser.add_argument("feature", help="Feature to identify/remove from project")
+    args = parser.parse_args()
+
+    #print("[+] Targeting project in: {} and feature: {}".format(args.project, args.feature))
+
+    if "mosquitto" in args.project:
+        makeMosquitto(args.project, args.feature)
+    elif "FFmpeg" in args.project:
+        makeFFmpeg(args.project, args.feature)
+    else:
+        print("[-] Target currently unsupported!")
+
+    sys.exit(0)
